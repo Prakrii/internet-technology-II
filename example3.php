db1379df448161e450f1fa8c4c6f8467a72f0460
Prakrit Acharya <prakrit.bss@gmail.com>
2025-09-03 09:03:26 +0545

    increment example and practice in PHP

---
+++ example3.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document III</title>
+</head>
+<body>
+    <?php
+    // example of an INCREMENT (outside a loop for a better understanding and practice)
+    $counter = 10; //defining a initial value
+    echo"<p> Initital counter : ".$counter." </p>"; // displaying using echo
+    $counter++; // adding 1 to counter
+    echo "<p> After increment :".$counter."</p>"; // displaying the counter after the increment
+    ?>
+</body>
+</html>
