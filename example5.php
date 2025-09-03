7940744654afa6ae8a34d7e0d6c66b6f00655740
Prakrit Acharya <prakrit.bss@gmail.com>
2025-09-03 09:24:34 +0545

    incomplete

---
+++ example5.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document V</title>
+</head>
+<body>
+    <?php
+    //Examples  of LOGICAL operators
+    $age = 25;
+    $isStudent = true;
+    //using AND operator
+    if ($age>18 && $isStudent){
+        echo "<p>Eligible for a student discount</p>";
+    }
+    //using OR operator
+    $rolledSix = false;
+    $rolledFive = true;
+    if ($rolledSix || $rolledFive){
+        echo "<p> you rolled 5 or a 6</p>"
+    }
+
+    //Using NOT operator 
+    $
+
+
+    ?>
+</body>
+</html>
