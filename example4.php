7efb6e482f2e2d415ae86bc930e86ccb19315418
Prakrit Acharya <prakrit.bss@gmail.com>
2025-09-03 09:10:59 +0545

    using assignment operators in PHP

---
+++ example4.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document IV</title>
+</head>
+<body>
+    <?php
+    //using operators in PHP
+    $message = "Hello";
+    $message .= ",World!"; // using concatenate and assign operator
+    echo "<p>".$message."<p>"; // displaying the combined output
+
+    $score = 50;
+    $score += 50; // adding 50 to score (add and assign operator)
+    echo "<p>".$score."<p>"
+    ?>
+</body>
+</html>
