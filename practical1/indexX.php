ed1bee7c1557d840c9a5103b0dca1afa592dc855
Prakrit Acharya <prakrit.bss@gmail.com>
2025-11-04 08:29:59 +0545

    init

---
+++ practical1/indexX.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>1.10</title>
+</head>
+<body>
+    
+ //  1.10 Create variables $firstName, $middleName, and $lastName, concatenate them into $fullName, and echo a greeting like "Hello, [fullName]!".
+<?php 
+
+
+$firstName = "Ram ";
+$middleName = "Kumar ";
+$lastName = "Thapa";
+
+$fullName = $firstName.$middleName.$lastName;
+
+echo "Hello, $fullName !";
+
+?>
+    
+</body>
+</html>
