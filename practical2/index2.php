5725e7b248ecbc0ffc695afb6cb2af748d5619a5
Prakrit Acharya <prakrit.bss@gmail.com>
2025-11-04 08:56:58 +0545

    init

---
+++ practical2/index2.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>2.2</title>
+</head>
+<body>
+    <!-- 2 Use if…else to display “Even” if a number is divisible by 2, otherwise “Odd.” -->
+    <?php
+    $num = 50 ;
+    if ($num%2==0){
+        echo"$num is even";
+    }
+    else{
+        echo"$num is odd";
+    }
+
+    
+    ?>
+</body>
+</html>
