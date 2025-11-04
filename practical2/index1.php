3c757ec5f6bef9b959c4a65a409a4eb465ce93da
Prakrit Acharya <prakrit.bss@gmail.com>
2025-11-04 08:56:51 +0545

    init

---
+++ practical2/index1.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>2.1</title>
+</head>
+<body>
+    // 2.1 Write an if statement to check if a number is greater than 100.
+    <?php
+    
+    $num = 50;
+    if ($num>100){
+        echo "$num is greater than 100";
+    }
+    else{
+        echo"$num is less than 100";
+    }
+
+    ?>
+</body>
+</html>
