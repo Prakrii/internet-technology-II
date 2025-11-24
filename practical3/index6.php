cb68e065744d33156d93fe39c4b93441fb8b6b8d
Prakrit Acharya <prakrit.bss@gmail.com>
2025-11-24 08:46:56 +0545

    init

---
+++ practical3/index6.php
@@ -0,0 +1,27 @@
+<!-- 6. Convert the string `"tag1,tag2,tag3"` to an array and back to `"tag1 | tag2 | tag3"` -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>3.6</title>
+</head>
+<body>
+    <?php
+
+
+        $str = "tag1,tag2,tag3";
+
+        // Convert string to array
+
+        $arr = explode(",", $str);
+
+        // Convert array back to formatted string
+
+        $result = implode(" | ", $arr);
+
+        echo $result;      // tag1 | tag2 | tag3
+
+    ?>
+</body>
+</html>
