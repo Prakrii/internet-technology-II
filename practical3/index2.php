0ea9a10b923210fc9d316289c54198f369a6f38e
Prakrit Acharya <prakrit.bss@gmail.com>
2025-11-24 08:24:18 +0545

    init

---
+++ practical3/index2.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>3.2</title>
+</head>
+<body>
+    <?php
+    $product = [
+    'name' => 'Laptop',
+    'price' => 1200,
+    'quality' => 'A',
+    'color'=>'red'
+    ];
+    foreach($product as $x){
+        echo $x ."<br>";
+    }
+
+    ?>
+</body>
+</html>
