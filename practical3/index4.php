07df1f5fe480853d34ce17d752c64f91cf657c27
Prakrit Acharya <prakrit.bss@gmail.com>
2025-11-24 08:39:12 +0545

    init

---
+++ practical3/index4.php
@@ -0,0 +1,15 @@
+<!-- 4. Get the length of `"PHP Code"` and convert it to uppercase. -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>3.4</title>
+</head>
+<body>
+    <?php
+    $string = "PHP code";
+    echo strtoupper($string);
+    ?>
+</body>
+</html>
