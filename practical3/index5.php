b790e54cecfd08253670da3d071f4c71085f502d
Prakrit Acharya <prakrit.bss@gmail.com>
2025-11-24 08:42:19 +0545

    init

---
+++ practical3/index5.php
@@ -0,0 +1,14 @@
+<!-- 5. Securely display the HTML string `"<p>Hello World!</p>"`. -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>3.5</title>
+</head>
+<body>
+    <?php
+    echo "<p>Hello World!</p>";
+    ?>
+</body>
+</html>
